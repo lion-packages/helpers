52c6a113c94f8739f5a5eb9cdcdd67f39e4fa132
Sleon4 <sergioleon4004@hotmail.com>
2024-04-10 09:49:07 -0500

    chore: annotations have been added to document the code

--- src/LionHelpers/Arr.php
+++ src/LionHelpers/Arr.php
@@ -8,13 +8,27 @@ use Closure;
 use InvalidArgumentException;
 use Lion\Helpers\Str;
 
+/**
+ * Modify and build arrays with different indexes or values.
+ *
+ * @var array|object $items [Array containing current values]
+ *
+ * @package Lion\Helpers
+ */
 class Arr
 {
+    /**
+     * [Array containing current values]
+     *
+     * @var array|object $items
+     */
     private array|object $items = [];
 
     /**
      * Resets the class property to its original value
-     * */
+     *
+     * @return void
+     */
     private function clean(): void
     {
         $this->items = [];
@@ -22,7 +36,9 @@ class Arr
 
     /**
      * Convert object to current array
-     * */
+     *
+     * @return Arr
+     */
     public function toObject(): Arr
     {
         $this->items = (object) $this->items;
@@ -32,7 +48,9 @@ class Arr
 
     /**
      * Get the indexes of the current array
-     * */
+     *
+     * @return Arr
+     */
     public function keys(): Arr
     {
         $this->items = array_keys($this->items);
@@ -42,7 +60,9 @@ class Arr
 
     /**
      * Get the values of the current array
-     * */
+     *
+     * @return Arr
+     */
     public function values(): Arr
     {
         $this->items = array_values($this->items);
@@ -52,10 +72,13 @@ class Arr
 
     /**
      * Gets the current value
-     * */
+     *
+     * @return array|object
+     */
     public function get(): array|object
     {
         $items = $this->items;
+
         $this->clean();
 
         return $items;
@@ -63,7 +86,12 @@ class Arr
 
     /**
      * Add an element to the current array
-     * */
+     *
+     * @param mixed $value [Value added to the array]
+     * @param int|string $key [Index with which the value is added to the array]
+     *
+     * @return Arr
+     */
     public function push(mixed $value, int|string $key = ''): Arr
     {
         if ('' === $key) {
@@ -77,7 +105,11 @@ class Arr
 
     /**
      * Set the defined value as current value
-     * */
+     *
+     * @param array $items [Array containing current values]
+     *
+     * @return Arr
+     */
     public function of(array $items): Arr
     {
         $this->items = $items;
@@ -87,18 +119,28 @@ class Arr
 
     /**
      * Gets the number of characters in the current string
-     * */
+     *
+     * @return int
+     */
     public function length(): int
     {
         return count($this->items);
     }
 
     /**
-     * Joins the values of the current array in string format using the defined delimiter
-     * */
+     * Joins the values of the current array in string format using the defined
+     * delimiter
+     *
+     * @param string $separator [Text that separates the texts in the string]
+     * @param null|string $lastSeparator [Text that separates the texts in the
+     * string in the last part]
+     *
+     * @return string
+     */
     public function join(string $separator = ', ', ?string $lastSeparator = null): string
     {
         $items = $this->items;
+
         $this->clean();
 
         if (null === $lastSeparator) {
@@ -116,7 +158,11 @@ class Arr
 
     /**
      * Uses the value of a column of a matrix as the key of the same matrix
-     * */
+     *
+     * @param string $column [Column name]
+     *
+     * @return Arr
+     */
     public function keyBy(string $column): Arr
     {
         $newItems = [];
@@ -137,7 +183,11 @@ class Arr
     /**
      * It uses the value of a column of an array as a key by creating a new
      * array and adding all arrays that share the same column value
-     * */
+     *
+     * @param string $column [Column name]
+     *
+     * @return Arr
+     */
     public function tree(string $column): Arr
     {
         $newItems = [];
@@ -165,17 +215,26 @@ class Arr
 
     /**
      * Adds a defined value to the start of the current array
-     * */
-    public function prepend(string $item, string $key = ''): Arr
+     *
+     * @param string $value [Value to add to current array]
+     * @param string $key [Index to add to current array]
+     *
+     * @return Arr
+     */
+    public function prepend(string $value, string $key = ''): Arr
     {
-        $this->items = '' === $key ? [$item, ...$this->items] : [$key => $item, ...$this->items];
+        $this->items = '' === $key ? [$value, ...$this->items] : [$key => $value, ...$this->items];
 
         return $this;
     }
 
     /**
      * Select a number of random elements from an array
-     * */
+     *
+     * @param int $limit [Number of elements obtained from the current array]
+     *
+     * @return Arr
+     */
     public function random(int $limit = 1): Arr
     {
         $size = $this->length();
@@ -211,7 +270,12 @@ class Arr
 
     /**
      * Gets a new array of elements based on its condition
-     * */
+     *
+     * @param Closure $callback [Executes a function that determines whether an
+     * element is added to the current array]
+     *
+     * @return Arr
+     */
     public function where(Closure $callback): Arr
     {
         $newItems = [];
@@ -229,7 +293,9 @@ class Arr
 
     /**
      * Gets a new array of elements where the values are neither null nor empty
-     * */
+     *
+     * @return Arr
+     */
     public function whereNotEmpty(): Arr
     {
         $str = new Str();
@@ -252,10 +318,13 @@ class Arr
 
     /**
      * Gets the first element of the current array
-     * */
+     *
+     * @return mixed
+     */
     public function first(): mixed
     {
         $value = reset($this->items);
+
         $this->clean();
 
         return $value;
@@ -263,10 +332,13 @@ class Arr
 
     /**
      * Gets the last element of the current array
-     * */
+     *
+     * @return mixed
+     */
     public function last(): mixed
     {
         $new_arr = end($this->items);
+
         $this->clean();
 
         return $new_arr;
@@ -274,7 +346,11 @@ class Arr
 
     /**
      * Create a new array with a value inside
-     * */
+     *
+     * @param mixed $value [Value that is added to the current array]
+     *
+     * @return Arr
+     */
     public function wrap(mixed $value = null): Arr
     {
         $this->items = empty($value) ? [] : [$value];
--- src/LionHelpers/Str.php
+++ src/LionHelpers/Str.php
@@ -6,367 +6,506 @@ namespace Lion\Helpers;
 
 use Lion\Helpers\Arr;
 
+/**
+ * Modify and construct strings with different formats
+ *
+ * @property null|string $word [Take the constructed text]
+ *
+ * @package Lion\Helpers
+ */
 class Str
 {
-	private ?string $word = '';
-
-	/**
-	 * Resets the class property to its original value
-	 * */
-	private function clean(): void
-	{
-		$this->word = '';
-	}
-
-	/**
-	 * Replaces defined characters
-	 * */
-	private function replaceChars(string $type, array $chars = []): string
-	{
-		$itemDefaultChar = '';
-		$itemReplaceChar = '';
-
-		if ($type === 'kebab') {
-			$itemDefaultChar = "_";
-			$itemReplaceChar = "-";
-			array_push($chars, ' ');
-		} elseif ($type === 'snake') {
-			$itemDefaultChar = "-";
-			$itemReplaceChar = "_";
-			array_push($chars, ' ');
-		} else {
-			$itemReplaceChar = " ";
-			array_push($chars, '-', '_', '/', '\\', '*', '+', '=', '|', '[', ']', '(', ')');
-		}
-
-		array_push($chars, $itemDefaultChar);
-
-		if (count($chars) > 0) {
-			foreach ($chars as $char) {
-				$this->replace($char, $itemReplaceChar);
-			}
-		}
-
-		return strtolower($this->word);
-	}
-
-	/**
-	 * Gets the current value
-	 * */
-	public function get(): ?string
-	{
-		$word = $this->word;
-		$this->clean();
-
-		return $word;
-	}
-
-	/**
-	 * Set the defined value as current value
-	 * */
-	public function of(?string $word): Str
-	{
-		$this->word = $word;
-
-		return $this;
-	}
-
-	/**
-	 * Separates the string into parts with the defined characters
-	 * */
-	public function split(string $split): array
-	{
-		return explode($split, $this->word);
-	}
-
-	/**
-	 * Add spaces to current string
-	 * */
-	public function spaces(int $spaces = 1): Str
-	{
-		for ($i = 0; $i < $spaces; $i++) {
-			$this->word .= " ";
-		}
-
-		return $this;
-	}
-
-	/**
-	 * Replaces the defined value with another value in the current string
-	 * */
-	public function replace($searchItem, $replaceItem): Str
-	{
-		$this->word = str_replace($searchItem, $replaceItem, $this->word);
-
-		return $this;
-	}
-
-	/**
-	 * Adds the defined text to the beginning of the string
-	 * */
-	public function prepend(string $prepend): Str
-	{
-		$this->word = $prepend . $this->word;
-
-		return $this;
-	}
-
-	/**
-	 * Add a line break to the current string
-	 * */
-	public function ln(): Str
-	{
-		$this->word .=  "\n";
-
-		return $this;
-	}
-
-	/**
-	 * Add a tab to the current string
-	 * */
-	public function lt(): Str
-	{
-		$this->word .=  "\t";
-
-		return $this;
-	}
-
-	/**
-	 * Converts the current string to null or returns the same value
-	 * if the current value is different from null
-	 * */
-	public function toNull(): Str
-	{
-		if (null === $this->word) {
-			$this->word = null;
-
-			return $this;
-		}
-
-		$this->word = empty(trim($this->word)) ? null : $this->word;
-
-		return $this;
-	}
-
-	/**
-	 * Gets the text contained before the defined word
-	 * */
-	public function before(string $delimiter): Str
-	{
-		$this->word = (new Arr())->of(explode($delimiter, $this->word))->first();
-		$this->toNull();
-
-		return $this;
-	}
-
-	/**
-	 * Gets the text contained after the defined word
-	 * */
-	public function after(string $delimiter): Str
-	{
-		$this->word = (new Arr())->of(explode($delimiter, $this->word))->last();
-		$this->toNull();
-
-		return $this;
-	}
-
-	/**
-	 * Gets the text contained between the defined words
-	 * */
-	public function between($firstDelimiter, $secondDelimiter): Str
-	{
-		$this->after($firstDelimiter);
-		$this->before($secondDelimiter);
-
-		return $this;
-	}
-
-	/**
-	 * Convert current string to Camel format
-	 * */
-	public function camel(): Str
-	{
-		$this->word = lcfirst(str_replace(" ", '', ucwords(strtolower($this->word))));
-
-		return $this;
-	}
-
-	/**
-	 * Convert current string to Pascal format
-	 * */
-	public function pascal(): Str
-	{
-		$this->word = str_replace(" ", '', ucwords(strtolower($this->word)));
-
-		return $this;
-	}
-
-	/**
-	 * Convert current string to Snake format
-	 * */
-	public function snake(array $chars = []): Str
-	{
-		$this->word = $this->replaceChars('snake', $chars);
-
-		return $this;
-	}
-
-	/**
-	 * Convert current string to Kebab format
-	 * */
-	public function kebab(array $chars = []): Str
-	{
-		$this->word = $this->replaceChars('kebab', $chars);
-
-		return $this;
-	}
-
-	/**
-	 * Adds formatting to the current string (initial letters in uppercase)
-	 * */
-	public function headline(): Str
-	{
-		$this->word = ucwords($this->replaceChars('all'));
-
-		return $this;
-	}
-
-	/**
-	 * Gets the number of characters in the current string
-	 * */
-	public function length(): int
-	{
-		return strlen($this->word);
-	}
-
-	/**
-	 * gets the first characters of the defined quantity of the current
-	 * string as new current string
-	 * */
-	public function limit(int $limit = 10): Str
-	{
-		$newStr = '';
-		$length = $this->length();
-		$limit = $limit > $length ? $length : $limit;
-
-		for ($i = 0; $i < $limit; $i++) {
-			$newStr .= $this->word[$i];
-		}
-
-		$this->word = $newStr;
-
-		return $this;
-	}
-
-	/**
-	 * Convert current string to lowercase
-	 * */
-	public function lower(): Str
-	{
-		$this->word = strtolower($this->word);
-
-		return $this;
-	}
-
-	/**
-	 * Convert current string to uppercase
-	 * */
-	public function upper(): Str
-	{
-		$this->word = strtoupper($this->word);
-
-		return $this;
-	}
-
-	/**
-	 * Converts part of the string obtained in a range of characters
-	 * to specific characters
-	 * */
-	public function mask(string $char, int $ignore): Str
-	{
-		$newStr = '';
-		$size = $this->length();
-		$ignoreSize = $size;
-
-		if ($ignore < 0) {
-			$ignoreSize = $ignoreSize - abs($ignore);
-		}
-
-		for ($i = 0; $i < $size; $i++) {
-			if ($ignore > 0) {
-				$newStr .= $i < $ignore ? $this->word[$i] : $char;
-			} else {
-				$newStr .= $i < $ignoreSize ? $char : $this->word[$i];
-			}
-		}
-
-		$this->word = $newStr;
-
-		return $this;
-	}
-
-	/**
-	 * Checks if the current string contains a certain number of defined words
-	 * */
-	public function contains(array $words): bool
-	{
-		foreach ($words as $key => $word) {
-			if (!preg_match("/{$word}/i", $this->word)) {
-				return false;
-				break;
-			}
-		}
-
-		return true;
-	}
-
-	/**
-	 * Replaces words in the current string with new words
-	 * */
-	public function swap(array $swaps): Str
-	{
-		$newStr = '';
-		$i = 0;
-
-		foreach ($swaps as $key => $swap) {
-			if ($i === 0) {
-				$i++;
-				$newStr = str_replace($key, $swap, $this->word);
-			} else {
-				$newStr = str_replace($key, $swap, $newStr);
-			}
-		}
-
-		$this->word = $newStr;
-
-		return $this;
-	}
-
-	/**
-	 * Tests the current string with a regular expression
-	 * */
-	public function test(string $test): bool
-	{
-		return (bool) preg_match($test, $this->word);
-	}
-
-	/**
-	 * Clears spaces at the beginning and end of the current string
-	 * */
-	public function trim(string $trim = ''): Str
-	{
-		$this->word = '' === $trim ? trim($this->word) : trim(str_replace($trim, '', $this->word));
-
-		return $this;
-	}
-
-	/**
-	 * Concatenate strings to the current string
-	 * */
-	public function concat(string $word): Str
-	{
-		$this->word .= $word;
-
-		return $this;
-	}
+    /**
+     * [Defines the minimum number of string characters]
+     *
+     * @const CHARACTER_LIMIT
+     */
+    private const CHARACTER_LIMIT = 10;
+
+    /**
+     * [Take the constructed text]
+     *
+     * @var null|string $word
+     */
+    private ?string $word = '';
+
+    /**
+     * Set the defined value as current value
+     *
+     * @param null|string $word [Take the constructed text]
+     *
+     * @return Str
+     */
+    public function of(?string $word): Str
+    {
+        $this->word = $word;
+
+        return $this;
+    }
+
+    /**
+     * Gets the current value
+     *
+     * @return null|string
+     */
+    public function get(): ?string
+    {
+        $word = $this->word;
+
+        $this->clean();
+
+        return $word;
+    }
+
+    /**
+     * Separates the string into parts with the defined characters
+     *
+     * @param string $split [Character that separates the string into parts]
+     *
+     * @return array<string>
+     */
+    public function split(string $split): array
+    {
+        return explode($split, $this->word);
+    }
+
+    /**
+     * Add spaces to current string
+     *
+     * @param int $spaces [Number of spaces that are nested in the current
+     * string]
+     *
+     * @return Str
+     */
+    public function spaces(int $spaces = 1): Str
+    {
+        for ($i = 0; $i < $spaces; $i++) {
+            $this->word .= " ";
+        }
+
+        return $this;
+    }
+
+    /**
+     * Replaces the defined value with another value in the current string
+     *
+     * @param string $searchItem [Text searched to be replaced]
+     * @param string $replaceItem [Text to replace string with another string]
+     *
+     * @return Str
+     */
+    public function replace(string $searchItem, string $replaceItem): Str
+    {
+        $this->word = str_replace($searchItem, $replaceItem, $this->word);
+
+        return $this;
+    }
+
+    /**
+     * Adds the defined text to the beginning of the string
+     *
+     * @param string $prepend [String that is added to the start of the current
+     * string]
+     *
+     * @return Str
+     */
+    public function prepend(string $prepend): Str
+    {
+        $this->word = $prepend . $this->word;
+
+        return $this;
+    }
+
+    /**
+     * Add a line break to the current string
+     *
+     * @return Str
+     */
+    public function ln(): Str
+    {
+        $this->word .=  "\n";
+
+        return $this;
+    }
+
+    /**
+     * Add a tab to the current string
+     *
+     * @return Str
+     */
+    public function lt(): Str
+    {
+        $this->word .=  "\t";
+
+        return $this;
+    }
+
+    /**
+     * Converts the current string to null or returns the same value
+     * if the current value is different from null
+     *
+     * @return Str
+     */
+    public function toNull(): Str
+    {
+        if (null === $this->word) {
+            $this->word = null;
+
+            return $this;
+        }
+
+        $this->word = empty(trim($this->word)) ? null : $this->word;
+
+        return $this;
+    }
+
+    /**
+     * Gets the text contained before the defined word
+     *
+     * @param string $delimiter [Defines the text that is returned before other
+     * text]
+     *
+     * @return Str
+     */
+    public function before(string $delimiter): Str
+    {
+        $this->word = (new Arr())->of(explode($delimiter, $this->word))->first();
+
+        $this->toNull();
+
+        return $this;
+    }
+
+    /**
+     * Gets the text contained after the defined word
+     *
+     * @param string $delimiter [Defines the text that is obtained after other
+     * text]
+     *
+     * @return Str
+     */
+    public function after(string $delimiter): Str
+    {
+        $this->word = (new Arr())->of(explode($delimiter, $this->word))->last();
+
+        $this->toNull();
+
+        return $this;
+    }
+
+    /**
+     * Gets the text contained between the defined words
+     *
+     * @param string $after [Defines the text that is obtained after
+     * other text]
+     * @param string $before [Defines the text that is returned before other
+     * text]
+     *
+     * @return Str
+     */
+    public function between(string $after, string $before): Str
+    {
+        $this->after($after);
+
+        $this->before($before);
+
+        return $this;
+    }
+
+    /**
+     * Convert current string to Camel format
+     *
+     * @return Str
+     */
+    public function camel(): Str
+    {
+        $this->word = lcfirst(str_replace(" ", '', ucwords(strtolower($this->word))));
+
+        return $this;
+    }
+
+    /**
+     * Convert current string to Pascal format
+     *
+     * @return Str
+     */
+    public function pascal(): Str
+    {
+        $this->word = str_replace(" ", '', ucwords(strtolower($this->word)));
+
+        return $this;
+    }
+
+    /**
+     * Convert current string to Snake format
+     *
+     * @param array<string> $chars [List of characters to be removed]
+     *
+     * @return Str
+     */
+    public function snake(array $chars = []): Str
+    {
+        $this->word = $this->replaceChars('snake', $chars);
+
+        return $this;
+    }
+
+    /**
+     * Convert current string to Kebab format
+     *
+     * @param array<string> $chars [List of characters to be removed]
+     *
+     * @return Str
+     */
+    public function kebab(array $chars = []): Str
+    {
+        $this->word = $this->replaceChars('kebab', $chars);
+
+        return $this;
+    }
+
+    /**
+     * Adds formatting to the current string (initial letters in uppercase)
+     *
+     * @return Str
+     */
+    public function headline(): Str
+    {
+        $this->word = ucwords($this->replaceChars('all'));
+
+        return $this;
+    }
+
+    /**
+     * Gets the number of characters in the current string
+     *
+     * @return int
+     */
+    public function length(): int
+    {
+        return strlen($this->word);
+    }
+
+    /**
+     * Gets the first characters of the defined quantity of the current string
+     * as the new current string
+     *
+     * @param int $limit [Limit of obtained characters]
+     *
+     * @return Str
+     */
+    public function limit(int $limit = self::CHARACTER_LIMIT): Str
+    {
+        $newStr = '';
+
+        $length = $this->length();
+
+        $limit = $limit > $length ? $length : $limit;
+
+        for ($i = 0; $i < $limit; $i++) {
+            $newStr .= $this->word[$i];
+        }
+
+        $this->word = $newStr;
+
+        return $this;
+    }
+
+    /**
+     * Convert current string to lowercase
+     *
+     * @return Str
+     */
+    public function lower(): Str
+    {
+        $this->word = strtolower($this->word);
+
+        return $this;
+    }
+
+    /**
+     * Convert current string to uppercase
+     *
+     * @return Str
+     */
+    public function upper(): Str
+    {
+        $this->word = strtoupper($this->word);
+
+        return $this;
+    }
+
+    /**
+     * Converts part of the string obtained in a range of characters
+     * to specific characters
+     *
+     * @param string $char [Character with which characters are replaced]
+     * @param int $ignore [Number of characters ignored]
+     *
+     * @return Str
+     */
+    public function mask(string $char = '*', int $ignore = self::CHARACTER_LIMIT): Str
+    {
+        $newStr = '';
+
+        $size = $this->length();
+
+        $ignoreSize = $size;
+
+        if ($ignore < 0) {
+            $ignoreSize = $ignoreSize - abs($ignore);
+        }
+
+        for ($i = 0; $i < $size; $i++) {
+            if ($ignore > 0) {
+                $newStr .= $i < $ignore ? $this->word[$i] : $char;
+            } else {
+                $newStr .= $i < $ignoreSize ? $char : $this->word[$i];
+            }
+        }
+
+        $this->word = $newStr;
+
+        return $this;
+    }
+
+    /**
+     * Checks if the current string contains a certain number of defined words
+     *
+     * @param array<string> $words [List of words]
+     *
+     * @return bool
+     */
+    public function contains(array $words): bool
+    {
+        foreach ($words as $word) {
+            if (!((bool) preg_match("/{$word}/i", $this->word))) {
+                return false;
+
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Replaces words in the current string with new words
+     *
+     * @param array<string, string> $swaps [List of words]
+     *
+     * @return Str
+     */
+    public function swap(array $swaps): Str
+    {
+        $newStr = '';
+
+        $i = 0;
+
+        foreach ($swaps as $key => $swap) {
+            if ($i === 0) {
+                $i++;
+
+                $newStr = str_replace($key, $swap, $this->word);
+            } else {
+                $newStr = str_replace($key, $swap, $newStr);
+            }
+        }
+
+        $this->word = $newStr;
+
+        return $this;
+    }
+
+    /**
+     * Tests the current string with a regular expression
+     *
+     * @param string $test [Regular expression for validation]
+     *
+     * @return bool
+     */
+    public function test(string $test): bool
+    {
+        return (bool) preg_match($test, $this->word);
+    }
+
+    /**
+     * Clears spaces at the beginning and end of the current string
+     *
+     * @param string $replace [Remove a value from the string]
+     *
+     * @return Str
+     */
+    public function trim(string $replace = ''): Str
+    {
+        $this->word = '' === $replace ? trim($this->word) : trim(str_replace($replace, '', $this->word));
+
+        return $this;
+    }
+
+    /**
+     * Concatenate strings to the current string
+     *
+     * @param string $word [Nest the text to the current string]
+     *
+     * @return Str
+     */
+    public function concat(string $word): Str
+    {
+        $this->word .= $word;
+
+        return $this;
+    }
+
+    /**
+     * Resets the class property to its original value
+     *
+     * @return void
+     */
+    private function clean(): void
+    {
+        $this->word = '';
+    }
+
+    /**
+     * Replaces defined characters
+     *
+     * @param string $type [Defines the type of format (Pascal, Camel, Snake,
+     * Kebab)]
+     * @param array $chars [List of characters to be removed]
+     *
+     * @return string
+     */
+    private function replaceChars(string $type, array $chars = []): string
+    {
+        $itemDefaultChar = '';
+
+        $itemReplaceChar = '';
+
+        if ($type === 'kebab') {
+            $itemDefaultChar = "_";
+
+            $itemReplaceChar = "-";
+
+            array_push($chars, ' ');
+        } elseif ($type === 'snake') {
+            $itemDefaultChar = "-";
+
+            $itemReplaceChar = "_";
+
+            array_push($chars, ' ');
+        } else {
+            $itemReplaceChar = " ";
+
+            array_push($chars, '-', '_', '/', '\\', '*', '+', '=', '|', '[', ']', '(', ')');
+        }
+
+        array_push($chars, $itemDefaultChar);
+
+        foreach ($chars as $char) {
+            $this->replace($char, $itemReplaceChar);
+        }
+
+        return strtolower($this->word);
+    }
 }
